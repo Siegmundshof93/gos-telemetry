d98d0e2bc754208bf9eb0a23fba4e2e1d25d398c
pvl <pav77788@gmail.com>
2019-06-13 00:52:06 +0200

    gui + bild

--- gui.py
+++ gui.py
@@ -1,29 +1,36 @@
 import os
 import tkinter as tk
 from tkinter import ttk
+import PIL
+from PIL import ImageTk, Image
 
 root = tk.Tk()
 
-canvas1 = tk.Canvas(root, width = 300, height = 350, bg = 'gray22', relief = 'raised')
+canvas1 = tk.Canvas(root, width = 321, height = 100, bg = 'gray1', relief = 'raised')
 canvas1.pack()
 
-
+ft = ttk.Frame()
+#function to call python file
 def firstFile ():
     global ein
     ein = 'python3 csvread.py '
     
     os.system(ein)
-
+    
+#button
   
-button1 = tk.Button(text='      run    ', command=firstFile, bg='gold4', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(150, 180, window=button1)
+button1 = tk.Button(text='      run    ', command=firstFile, bg='burlywood3', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 50, window=button1)
+
+
+#image
+
+img = ImageTk.PhotoImage(Image.open('/home/pvl/GOS/gos-telemetry/GOS1.png'))
+panel = tk.Label(root, image = img)
+panel.pack(side = "bottom", fill = "both", expand = "no")
 
 
 
-mpb = ttk.Progressbar(root,orient ="horizontal",length = 200, mode ="determinate")
-mpb.pack()
-mpb["maximum"] = 100
-mpb["value"] = 50
-
+    
 
 root.mainloop()
